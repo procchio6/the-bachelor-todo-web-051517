b44efb0a24afc5b6fe9f3ba9d3fc207bb5b25fc8
Pasquale Rocchio <patrocc6@gmail.com>
2017-05-05 23:31:55 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find { |contestant| contestant["status"] == "Winner" }
+  winner["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_with_occupation = ""
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        contestant_with_occupation = contestant["name"]
+      end
+    end
+  end
+  contestant_with_occupation
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        occupation = contestant["occupation"]
+        break
+      end
+    end
+  end
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |current_season, contestants|
+    if current_season == season
+      contestants.each do |contestant|
+        ages << contestant["age"].to_f
+      end
+    end
+  end
+  average_age = (ages.inject(:+) / ages.count).round
 end
